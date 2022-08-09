f217042347a25e0725df7f85490e1b72f3909480
Bogdan <pozdnyakov.bogdan00@gmail.com>
2022-08-09 17:08:37 +0600

    Added task two

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Exercise 2. 
+
+Console.WriteLine("Enter NumberA:");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter NumberB:");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+string OutPutText;
+
+if (NumberA>NumberB)
+{
+  OutPutText = "Maximum number is NumberA ("+NumberA+"); Minimum number is NumberB ("+NumberB+").";  
+}
+else
+{
+    OutPutText = "Maximum number is NumberB ("+NumberB+"); Minimum number is NumberA ("+NumberA+").";   
+}
+
+Console.WriteLine(OutPutText);
