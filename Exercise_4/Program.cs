b2fff47a8bbd14cbefefa07ca43449dbb532f139
Bogdan <pozdnyakov.bogdan00@gmail.com>
2022-08-09 18:30:53 +0600

    Added task four

---
+++ Exercise_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Exercise 4
+int ? MaxNumber = null;
+for (int i = 1; i <= 3; i++)
+{
+    Console.WriteLine("Enter number № "+i+":");
+    int NumberI = Convert.ToInt32(Console.ReadLine());
+    if (MaxNumber == null ? true : NumberI > MaxNumber)
+    {
+        MaxNumber = NumberI;      
+    }  
+
+}
+Console.WriteLine("Max number is " + MaxNumber);
