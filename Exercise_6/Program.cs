0632eb330899dea00829106813991bd501158244
Bogdan <pozdnyakov.bogdan00@gmail.com>
2022-08-09 18:49:46 +0600

    Added task six

---
+++ Exercise_6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Exercise 6
+Console.WriteLine("Enter number: ");
+int InPutNumber = Convert.ToInt32(Console.ReadLine());
+string Result = "Number is uneven";
+
+if (InPutNumber %2 == 0)
+{
+    Result = "Number is even";   
+}
+
+Console.WriteLine(Result);
