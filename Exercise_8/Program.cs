bf0a3f5df703eba71cc90c0be930611e4161fc1b
Bogdan <pozdnyakov.bogdan00@gmail.com>
2022-08-09 19:01:38 +0600

    Added task eight

---
+++ Exercise_8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Exercise 8 
+Console.WriteLine("Enter N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = -N; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
